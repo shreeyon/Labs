4ecd5ba0490b895c1f92afa05c16b536bed842da
shreeyon <081bel079.shreeyon@pcampus.edu.np>
2025-08-14 07:21:08 +0545

    updated

--- LAB1/Conditions and loop/prime1.py
+++ LAB1/Conditions and loop/prime1.py
@@ -1,5 +1,5 @@
 #1. Write a program to check whether a given number is prime or not.
-a=int(input("Enter a number"))
+a=int(input("Enter a number: "))
 count=0
 for i in range(1,a+1):
     if(a%i==0):
